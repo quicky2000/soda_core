6b2bc544370682a8bc2333897f9ce1426162b942
Julien Thevenon <julien_thevenon@yahoo.fr>
2012-10-15 20:09:24 +0200

    Add display features

--- include/osm_changeset.h
+++ include/osm_changeset.h
@@ -8,6 +8,7 @@ namespace osm_diff_watcher
 {
   class osm_changeset: public osm_object
   {
+    friend std::ostream & operator<<(std::ostream & p_stream,const osm_changeset & p_changeset);
   public:
     inline osm_changeset(const t_osm_id  & p_id,
                          const t_osm_id & p_user_id,
@@ -21,13 +22,25 @@ namespace osm_diff_watcher
     inline const std::string & get_closed_at(void)const;
     inline bool is_open(void)const;
     inline const osm_bounding_box & get_bounding_box(void)const;
+    inline static const std::string & get_type_str(void);
   private:
     const std::string m_created_at;
     const std::string m_closed_at;
     bool m_open;
     osm_bounding_box m_bounding_box;
+    static const std::string m_type;
   }
   ;
+
+  //------------------------------------------------------------------------------
+  inline std::ostream & operator<<(std::ostream & p_stream,const osm_changeset & p_changeset)
+    {
+      p_stream << "changeset id=" << p_changeset.get_id() << " created_at=" << p_changeset.get_created_at() << " closed_at=" << p_changeset.get_closed_at() << " open=" << p_changeset.is_open() << std::endl ;
+      std::string l_tags_str;
+      p_changeset.tags_to_string(l_tags_str);
+      p_stream << l_tags_str ;
+      return p_stream;
+    }
 
   //----------------------------------------------------------------------------
   osm_changeset::osm_changeset(const t_osm_id  & p_id,
@@ -69,7 +82,12 @@ namespace osm_diff_watcher
       {
         return m_bounding_box;
       }
-  
+
+    //----------------------------------------------------------------------------
+    const std::string & osm_changeset::get_type_str(void)
+    {
+      return m_type;
+    }
 }
 #endif // _OSM_CHANGESET_H_
 //EOF
