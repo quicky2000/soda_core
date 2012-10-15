c40cd8577080e7ab34ac49c9d0f285e709e9d5b6
Julien Thevenon <julien_thevenon@yahoo.fr>
2012-10-15 20:09:40 +0200

    Add display features, method to convert change type to string

--- src/osm_change.cpp
+++ src/osm_change.cpp
@@ -3,6 +3,7 @@
 namespace osm_diff_watcher
 {
   std::map<std::string,osm_change::t_osm_change_type> osm_change::m_osm_change_types;
+  std::map<osm_change::t_osm_change_type,std::string> osm_change::m_osm_change_types_str;
 }
 
 //EOF
