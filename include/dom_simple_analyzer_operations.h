412485ebdc0ed840ab8006cda6436fd4f13db779
Julien Thevenon <julien_thevenon@yahoo.fr>
2012-11-01 22:39:09 +0100

    Adding licence information

--- include/dom_simple_analyzer_operations.h
+++ include/dom_simple_analyzer_operations.h
@@ -1,3 +1,23 @@
+/*
+    This file is part of osm_diff_watcher, Openstreetmap diff analyze framework
+    The aim of this software is to provided generic facilities for diff analyzis
+    to allow developers to concentrate on analyze rather than diff management 
+    infrastructure
+    Copyright (C) 2012  Julien Thevenon ( julien_thevenon at yahoo.fr )
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
 #ifndef _DOM_SIMPLE_ANALYZER_OPERATIONS_H_
 #define _DOM_SIMPLE_ANALYZER_OPERATIONS_H_
 
