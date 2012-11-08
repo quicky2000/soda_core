a33b118fe4d6baeceffaaa19288ae0c10928c424
Julien Thevenon <julien_thevenon@yahoo.fr>
2012-11-08 22:54:47 +0100

    Dom_osm_parser just perform a warning in case of empty XML files instead of error

--- include/dom_parser_base.h
+++ include/dom_parser_base.h
@@ -1,22 +1,22 @@
 /*
-    This file is part of osm_diff_watcher, Openstreetmap diff analyze framework
-    The aim of this software is to provided generic facilities for diff analyzis
-    to allow developers to concentrate on analyze rather than diff management 
-    infrastructure
-    Copyright (C) 2012  Julien Thevenon ( julien_thevenon at yahoo.fr )
+  This file is part of osm_diff_watcher, Openstreetmap diff analyze framework
+  The aim of this software is to provided generic facilities for diff analyzis
+  to allow developers to concentrate on analyze rather than diff management 
+  infrastructure
+  Copyright (C) 2012  Julien Thevenon ( julien_thevenon at yahoo.fr )
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 #ifndef _DOM_PARSER_BASE_H_
 #define _DOM_PARSER_BASE_H_
@@ -46,64 +46,70 @@ namespace osm_diff_watcher
 
   //------------------------------------------------------------------------------
   template <class T>
-  dom_parser_base<T>::dom_parser_base(const std::string & p_root):
+    dom_parser_base<T>::dom_parser_base(const std::string & p_root):
     m_root(p_root)
+    {
+    }
+    //------------------------------------------------------------------------------
+    template <class T>
+      void dom_parser_base<T>::parse_stream(std::istream & p_stream)
       {
+        std::string l_xml_string;
+        std::string l_line;
+        while(!getline(p_stream,l_line).eof())
+          {
+            l_xml_string += l_line;
+          }
+        parse_string(l_xml_string);
       }
-  //------------------------------------------------------------------------------
-  template <class T>
-  void dom_parser_base<T>::parse_stream(std::istream & p_stream)
-  {
-    std::string l_xml_string;
-    std::string l_line;
-    while(!getline(p_stream,l_line).eof())
+
+    //------------------------------------------------------------------------------
+    template <class T>
+      void dom_parser_base<T>::parse_string(const std::string & p_string)
       {
-	l_xml_string += l_line;
-      }
-    parse_string(l_xml_string);
-  }
+        if(p_string != " " && p_string != "")
+          {
+            XMLResults l_err= {eXMLErrorNone,0,0};
+            XMLNode l_node = XMLNode::parseString(p_string.c_str(),m_root.c_str(),&l_err);
+            std::string l_error_msg;
+            dom_parser_base<T>::error_message(l_err,l_error_msg);
+            if(l_error_msg != "")
+              {
+                std::cout << l_error_msg << std::endl ;
+                exit(-1);
+              }
+            std::cout << "DOM PARSE SUCCESSFULL" << std::endl ;
 
-  //------------------------------------------------------------------------------
-  template <class T>
-  void dom_parser_base<T>::parse_string(const std::string & p_string)
-  {
-    XMLResults l_err= {eXMLErrorNone,0,0};
-    XMLNode l_node = XMLNode::parseString(p_string.c_str(),m_root.c_str(),&l_err);
-    std::string l_error_msg;
-    dom_parser_base<T>::error_message(l_err,l_error_msg);
-    if(l_error_msg != "")
+            this->perform_init();
+            this->perform_dedicated_analyze(l_node);
+            std::cout << "End of DOM based analyzers" << std::endl ;
+          }
+        else
+          {
+            std::cout << "WARNING : empty XML" << std::endl ;
+          }
+      }
+
+    //------------------------------------------------------------------------------
+    template <class T>
+      void dom_parser_base<T>::error_message(const XMLResults & p_err,std::string & p_msg)
       {
-	std::cout << l_error_msg << std::endl ;
-	exit(-1);
+        p_msg = XMLNode::getError(p_err.error);
+        std::stringstream l_line_number ;
+        l_line_number << p_err.nLine;
+        std::stringstream l_column_number ;
+        l_column_number << p_err.nColumn;
+
+
+        if(p_msg != "No error")
+          {
+            p_msg = "dom_parser : \"" + p_msg+ "\" on line " + l_line_number.str() + " when parsing XML column " + l_column_number.str();
+          }
+        else
+          {
+            p_msg = "";
+          }
       }
-    std::cout << "DOM PARSE SUCCESSFULL" << std::endl ;
-
-    this->perform_init();
-    this->perform_dedicated_analyze(l_node);
-
-    std::cout << "End of DOM based analyzers" << std::endl ;
-  }
-
-  //------------------------------------------------------------------------------
-  template <class T>
-  void dom_parser_base<T>::error_message(const XMLResults & p_err,std::string & p_msg)
-  {
-    p_msg = XMLNode::getError(p_err.error);
-    std::stringstream l_line_number ;
-    l_line_number << p_err.nLine;
-    std::stringstream l_column_number ;
-    l_column_number << p_err.nColumn;
-
-
-    if(p_msg != "No error")
-      {
-	p_msg = "dom_parser : \"" + p_msg+ "\" on line " + l_line_number.str() + " when parsing XML column " + l_column_number.str();
-      }
-    else
-      {
-	p_msg = "";
-      }
-  }
 }
 
 #endif // _DOM_PARSER_BASE_H_
