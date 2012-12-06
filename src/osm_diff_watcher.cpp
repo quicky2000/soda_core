637893da9e1dd729470ef144834cf0f41e514a37
Julien Thevenon <julien_thevenon@yahoo.fr>
2012-12-06 19:22:36 +0100

    Improve diff consumption algorithm, now manage case where diff is still not available at start of run

--- src/osm_diff_watcher.cpp
+++ src/osm_diff_watcher.cpp
@@ -1,22 +1,22 @@
 /*
-    This file is part of osm_diff_watcher, Openstreetmap diff analyze framework
-    The aim of this software is to provided generic facilities for diff analyzis
-    to allow developers to concentrate on analyze rather than diff management 
-    infrastructure
-    Copyright (C) 2012  Julien Thevenon ( julien_thevenon at yahoo.fr )
+  This file is part of osm_diff_watcher, Openstreetmap diff analyze framework
+  The aim of this software is to provided generic facilities for diff analyzis
+  to allow developers to concentrate on analyze rather than diff management 
+  infrastructure
+  Copyright (C) 2012  Julien Thevenon ( julien_thevenon at yahoo.fr )
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 #include "osm_diff_watcher.h"
 #include "url_reader.h"
@@ -83,7 +83,7 @@ namespace osm_diff_watcher
               {
                 std::cout << "Invalid dom_analyzer \"" << l_type << "\"" << std::endl ;
                 exit(-1);
-               }
+              }
 	    m_dom_analyzers.insert(make_pair(l_dom_analyzer->get_name(),l_dom_analyzer));
 	  }
 	else if(l_input_type=="sax")
@@ -309,6 +309,9 @@ namespace osm_diff_watcher
       }
     time_t l_previous_diff_time;
     bool l_first_diff = true;
+ 
+    double l_remaining_time = 0;
+    double l_time_between_diff = 0;
     while(l_nb_iteration && !m_stop)
       {
 	std::cout << "--------------------------------------------------------" << std::endl ;
@@ -316,112 +319,111 @@ namespace osm_diff_watcher
           {
             std::cout << l_nb_iteration << " remaining iterations" << std::endl ;
           }
-	std::cout << "Latest seq number available = " << l_end_seq_number << std::endl ;
+	std::cout << "Latest seq number that was available = " << l_end_seq_number << std::endl ;
 	std::cout << "--> Sequence number = \"" << l_current_seq_number << "\"" << std::endl;
 
-	std::string l_state_url;
-	m_ressources.get_state_url_diff(l_state_url,l_current_seq_number);
-
-	if(l_current_seq_number != l_end_seq_number)
-	  {
-	    delete l_diff_state;
-            //TO DELETE	    l_diff_state = new osm_diff_analyzer_if::osm_diff_state(l_current_seq_number,"",m_ressources.get_replication_domain());
-            l_diff_state = m_ressources.get_minute_diff_state(l_state_url);
-	  }
-	else if(l_diff_state == NULL)
-	  {
-            //TO DELETE	    l_diff_state = new osm_diff_analyzer_if::osm_diff_state(l_current_seq_number,"",m_ressources.get_replication_domain());
-            l_diff_state = m_ressources.get_minute_diff_state(l_state_url);
-	  }
-        std::cout << "Timestamp of diff file : " << l_diff_state->get_timestamp() << std::endl ;
-        time_t l_diff_time = extract_time(l_diff_state->get_timestamp());
-
-        //Compute time elapsed between two diffs generations to adjust wait delay
-        double l_time_between_diff = 60;
-        if(!l_first_diff)
+        // Waiting for end sequence number
+        uint32_t l_delay = (uint32_t) l_remaining_time;
+        // In case of replication domain jump it is important to recompute end_seq_time so 
+        // using do while ensure that it will be recomputed at least once
+        bool l_first_loop = true;
+        do
           {
-            l_time_between_diff = difftime(l_diff_time,l_previous_diff_time); 
-          }
-        else
-          {
-            l_first_diff = false;
-          }
-        l_previous_diff_time = l_diff_time ;
+            if(!l_first_loop && l_delay)
+              {
+                std::cout << "Wait for " << l_delay << " seconds" << std::endl ;
+#ifndef _WIN32
+                sleep(l_delay);
+#else
+                Sleep(1000*l_delay);
+#endif
+              }
+            delete l_diff_state;
+            l_diff_state = m_ressources.get_minute_diff_state();
+            l_end_seq_number = l_diff_state->get_sequence_number();
+            
+            if(!l_first_loop)
+              {
+                // Increase delay in case of unchanged end seqence number
+                if(l_delay == (uint32_t) l_remaining_time) l_delay = 1;
+                if(l_delay < l_time_between_diff / 2 || l_time_between_diff == 0) l_delay *= 2;
+              }
+            l_first_loop = false;
+          }while(l_current_seq_number > l_end_seq_number && !m_stop);
 
-	std::string l_url_diff = m_ressources.get_url_minute_diff(l_current_seq_number);
-	std::cout << "Url of diff file \"" << l_url_diff << "\"" << std::endl ;
-        time_t l_begin_time = time(NULL);
-	bool l_404_error = false;
+        if(!m_stop)
+          {
+            if(l_current_seq_number != l_end_seq_number)
+              {
+                delete l_diff_state;
+                std::string l_state_url;
+                m_ressources.get_state_url_diff(l_state_url,l_current_seq_number);
+                l_diff_state = m_ressources.get_minute_diff_state(l_state_url);
+              }
+            std::cout << "Timestamp of diff file : " << l_diff_state->get_timestamp() << std::endl ;
+            time_t l_diff_time = extract_time(l_diff_state->get_timestamp());
+
+            //Compute time elapsed between two diffs generations to adjust wait delay
+            if(!l_first_diff)
+              {
+                l_time_between_diff = difftime(l_diff_time,l_previous_diff_time); 
+              }
+            else
+              {
+                l_first_diff = false;
+              }
+            l_previous_diff_time = l_diff_time ;
+
+            std::string l_url_diff = m_ressources.get_url_minute_diff(l_current_seq_number);
+            std::cout << "Url of diff file \"" << l_url_diff << "\"" << std::endl ;
+            time_t l_begin_time = time(NULL);
+            bool l_404_error = false;
 	
-	// Check if download has succeeded
-	uint32_t l_404_trial = 3;
-	do
-	  {
-	    m_url_reader.dump_url_binary(l_url_diff,m_diff_file_name);
-	    l_404_error = check_404_error(m_diff_file_name);
-	    --l_404_trial;
-	  } while(l_404_error && l_404_trial);
+            // Check if download has succeeded
+            uint32_t l_404_trial = 3;
+            do
+              {
+                m_url_reader.dump_url_binary(l_url_diff,m_diff_file_name);
+                l_404_error = check_404_error(m_diff_file_name);
+                --l_404_trial;
+              } while(l_404_error && l_404_trial);
 
-	if(l_404_error)
-	  {
-	    std::cout << "ERROR : download of " << l_url_diff << " failed to many times" << std::endl ;
-	    exit(-1);
-	  }
+            if(l_404_error)
+              {
+                std::cout << "ERROR : download of " << l_url_diff << " failed to many times" << std::endl ;
+                exit(-1);
+              }
 
-        // Parse diff file
-	parse_diff(l_diff_state);
+            // Parse diff file
+            parse_diff(l_diff_state);
         
-        // Remember reference of parsed diff
-	m_informations.store(*l_diff_state);
-        delete l_diff_state;
-        l_diff_state = NULL;
-	--l_nb_iteration;
+            // Remember reference of parsed diff
+            m_informations.store(*l_diff_state);
+            --l_nb_iteration;
 
-        // Prepare wait of next diff if necessary : latest available number is current sequence number
-	if(l_current_seq_number == l_end_seq_number && !m_stop && l_nb_iteration)
-	  {
+            // Prepare dely to wait next diff if necessary : latest available number is current sequence number and we are not at domain jump
+            l_remaining_time = 0;
             uint64_t l_next_seq_number = get_next_sequence_number(l_current_seq_number);
 
-            // Check if we are just before a domain jump. Not necessary to wai tin case of domain jump
-            if(l_next_seq_number == l_current_seq_number +1)
+            if(l_current_seq_number == l_end_seq_number && !m_stop && l_nb_iteration)
               {
-                // Determine time needed to run analyzer on diff file
-                time_t l_end_time = time(NULL);
-                double l_computation_time = difftime(l_end_time,l_begin_time);
-                std::cout << "Number of seconds between diffs : " << l_time_between_diff << " s" << std::endl ;
-                std::cout << "Time spent in analyze = " << l_computation_time << "s" << std::endl ;
+                // Check if we are just before a domain jump. Not necessary to wait in case of domain jump
+                if(l_next_seq_number == l_current_seq_number +1)
+                  {
+                    // Determine time needed to run analyzer on diff file
+                    time_t l_end_time = time(NULL);
+                    double l_computation_time = difftime(l_end_time,l_begin_time);
+                    std::cout << "Number of seconds between diffs : " << l_time_between_diff << " s" << std::endl ;
+                    std::cout << "Time spent in analyze = " << l_computation_time << "s" << std::endl ;
 
-                // Compute remaining time before next diff generation
-                double l_remaining_time = 0;
-                if(l_computation_time < l_time_between_diff)
-                  {
-                    l_remaining_time = l_time_between_diff - l_computation_time;
-                  }
-                // Waiting for new end sequence number
-                uint32_t l_delay = (uint32_t) l_remaining_time;
-                do
-                  {
-                    std::cout << "Wait for " << l_delay << " seconds" << std::endl ;
-#ifndef _WIN32
-                    sleep(l_delay);
-#else
-                    Sleep(1000*l_delay);
-#endif
-                    delete l_diff_state;
-                    l_diff_state = m_ressources.get_minute_diff_state();
-                    l_end_seq_number = l_diff_state->get_sequence_number();
-                    // Increase delay in case of unchanged end seqence number
-                    if(l_delay == (uint32_t) l_remaining_time) l_delay = 1;
-                    if(l_delay < l_time_between_diff / 2) l_delay *= 2;
-                  }while(l_current_seq_number == l_end_seq_number && !m_stop);
-              
+                    // Compute remaining time before next diff generation
+                    if(l_computation_time < l_time_between_diff)
+                      {
+                        l_remaining_time = l_time_between_diff - l_computation_time;
+                      }
+                  } 
               }
             l_current_seq_number = l_next_seq_number;
- 
-	  }
-        else
-          {
-            ++l_current_seq_number;
           }
       }
     delete l_diff_state;
